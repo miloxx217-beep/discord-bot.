d40e010fe9264c0486c907066501096b50ba5c5b
miloxx217-beep <miloxx217@gmail.com>
2026-03-07 19:34:55 +0100

    Create register-commands.js

---
+++ register-commands.js
@@ -0,0 +1,37 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+
+const commands = [
+
+    new SlashCommandBuilder()
+        .setName("pracuj")
+        .setDescription("Pracuj i zarób od 30 do 400 monet."),
+
+    new SlashCommandBuilder()
+        .setName("dodajkase")
+        .setDescription("Dodaj pieniądze użytkownikowi (tylko właściciel).")
+        .addUserOption(option =>
+            option.setName("uzytkownik")
+                .setDescription("Użytkownik, któremu chcesz dodać kasę")
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName("kwota")
+                .setDescription("Kwota do dodania")
+                .setRequired(true))
+]
+    .map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log("Rejestrowanie komend...");
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, "1478750576408793239"),
+            { body: commands }
+        );
+        console.log("Komendy zarejestrowane!");
+    } catch (err) {
+        console.error(err);
+    }
+})();
