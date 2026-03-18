878ca09cd40aadf30c1177c91cb43f6403ac10eb
miloxx217-beep <miloxx217@gmail.com>
2026-03-18 16:51:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,7 +123,6 @@ require("./autovoice.js")(client, shared);
 
 require("./ranking.js")(client);
 require("./mandaty.js")(client);
-require("./resetdowod.js")(client, shared);
 
 // ============================
 // START
